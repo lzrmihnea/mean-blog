f6795bf7be80d040816607a5aace6ca25da25e37
lzrmihnea <lzr_mihnea@yahoo.com>
2013-05-28 23:45:15 +0300

    Archive tree - part 4

--- routes/archive.js
+++ routes/archive.js
@@ -25,49 +25,59 @@ module.exports = function (app) {
 
 
 function fillTree(post) {
-    //BEFORE
+
     if (!tree1.years || typeof tree1.years == 'undefined') {
-        tree1 = {
-            years: [{
-                year: post.created.getFullYear(),
-                months: [
-                    {
-                        month: post.getFormattedMonth(),
-                        posts: [
-                            post
-                        ]
-                    }
-                ]
-            }]
-        };
+        modelTree();
         return;
     }
     else {
         for (var yearIndex = 0; yearIndex <= tree1.years.length; yearIndex++) {
+            if (!tree1.years[yearIndex]) {
+                addYearBranch();
+                return;
+            }
             if (tree1.years[yearIndex].year == post.created.getFullYear()) {
                 for (var monthIndex = 0; monthIndex <= tree1.years[yearIndex].months.length; monthIndex++) {
+                    if (!tree1.years[yearIndex].months[monthIndex]) {
+                        addMonthBranchToYear();
+                        return;
+                    }
                     if (tree1.years[yearIndex].months[monthIndex].month == post.getFormattedMonth()) {
-                        for (var postIndex = 0; postIndex <= tree1.years[yearIndex].months[monthIndex].posts.length; postIndex++) {
-                            if (!tree1.years[yearIndex].months[monthIndex].posts[postIndex]) {
-                                tree1.years[yearIndex].months[monthIndex].posts.push(post);
-                                return;
-                            }
-                        }
-                    } else {
-                        tree1.years[yearIndex].months.push({
-                            month: post.getFormattedMonth(),
-                            posts: [{post: post.title}]
-                        });
+                        addPostLeafToMonth();
                         return;
                     }
                 }
-            } else {
-                tree1.years.push({
-                    year: post.created.getFullYear(),
-                    months: [{month: post.getFormattedMonth(), posts: [{post: post.title}]}]
-                });
-                return;
             }
         }
+    }
+
+    // Inner functions
+
+    function modelTree() {
+        tree1 = {
+            years: [{
+                year: post.created.getFullYear(),
+                months: [
+                    {   month: post.getFormattedMonth(),
+                        posts: [ post ] }
+                ] }] };
+    }
+
+    function addYearBranch() {
+        tree1.years.push({
+            year: post.created.getFullYear(),
+            months: [{month: post.getFormattedMonth(), posts: [post]}]
+        });
+    }
+
+    function addMonthBranchToYear() {
+        tree1.years[yearIndex].months.push({
+            month: post.getFormattedMonth(),
+            posts: [post]
+        });
+    }
+
+    function addPostLeafToMonth() {
+        tree1.years[yearIndex].months[monthIndex].posts.push(post);
     }
 }
