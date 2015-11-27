0d88771b43515e4b6f6bc37eae25596fd5ccd052
lzrmihnea <lazarm@MSGN02135.int.root.msg.ag>
2015-11-27 02:18:48 +0200

    GitHub blog connects to read-only user

--- app0.js
+++ app0.js
@@ -10,15 +10,32 @@ var routes = require('./routes/index');
 var middleware = require('./middleware/index');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost', function(err){
+const readOnlyUser_MongoLab = 'mongodb://read-user:read-user-password@ds059634.mongolab.com:59634/croc-blog';
+
+mongoose.connect(readOnlyUser_MongoLab, function (err) {
     if (err) throw err;
 
     var app = express();
     middleware(app);
     routes(app);
 
-    app.listen(3000, function() {
-        console.log('now listening on http://localhost:3000');
+    const PORT_NUMBER = 3000;
+    app.listen(PORT_NUMBER, function () {
+        const LOCALHOST_LINK = 'http://localhost:' + PORT_NUMBER;
+        const IRINA_LOGHIN = '/irinaloghin';
+
+        writeInitialConsoleMessage();
+
+        function writeInitialConsoleMessage() {
+            addNewlineToConsole();
+            console.log('Blog address: ' + LOCALHOST_LINK);
+            addNewlineToConsole();
+            console.log('Login backdoor ' + LOCALHOST_LINK + IRINA_LOGHIN);
+            addNewlineToConsole();
+            function addNewlineToConsole() {
+                console.log();
+            }
+        }
     })
     //mongoose.disconnect();
 });
