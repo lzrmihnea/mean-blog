e2afea503dd5da3a84cb3bc8262747f896d830d7
lzrmihnea <lzr_mihnea@yahoo.com>
2015-11-28 16:10:29 +0200

    [Heroku] Added readWrite user to DB connection, so that readers can comment

--- app0.js
+++ app0.js
@@ -10,19 +10,23 @@ var routes = require('./routes/index');
 var middleware = require('./middleware/index');
 var mongoose = require('mongoose');
 
-const readOnlyUser_MongoLab = 'mongodb://read-user:read-user-password@ds059634.mongolab.com:59634/croc-blog';
+const readWriteUser_MongoLab = 'mongodb://readwrite-user:ReadWrite-Password0@ds059634.mongolab.com:59634/croc-blog';
 
-mongoose.connect(readOnlyUser_MongoLab, function (err) {
+mongoose.connect(readWriteUser_MongoLab, function (err) {
     if (err) throw err;
 
     var app = express();
     middleware(app);
     routes(app);
 
-    app.listen(process.env.PORT || 5000, function () {
+    var appPort = process.env.PORT;
+    if (appPort == undefined) {
+        appPort = 5000;
+    }
+    app.listen(appPort, function () {
 
         const SERVER_IP_ADDRESS = '127.0.0.1';
-        const LOCALHOST_LINK = SERVER_IP_ADDRESS + ":" + process.env.PORT || 5000;
+        const LOCALHOST_LINK = 'http://' + SERVER_IP_ADDRESS + ":" + appPort;
         const IRINA_LOGHIN = '/irinaloghin';
 
         writeInitialConsoleMessage();
@@ -31,7 +35,7 @@ mongoose.connect(readOnlyUser_MongoLab, function (err) {
             addNewlineToConsole();
             console.log('Blog address: ' + LOCALHOST_LINK);
             addNewlineToConsole();
-            console.log('Login backdoor ' + LOCALHOST_LINK + IRINA_LOGHIN);
+            console.log('Login backdoor: ' + LOCALHOST_LINK + IRINA_LOGHIN);
             addNewlineToConsole();
             function addNewlineToConsole() {
                 console.log();
