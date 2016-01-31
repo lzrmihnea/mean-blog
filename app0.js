2725df3bdb8789cdfab216ac3748b75a795ef48e
lzrmihnea <lzr_mihnea@yahoo.com>
2016-01-31 22:25:53 +0200

    [SSL] Attempt at SSL validation

--- app0.js
+++ app0.js
@@ -2,13 +2,11 @@
  * Created by Mihnea on 5/1/2015.
  */
 var express = require('express');
-
-// Add Mongoose query and promise to support to express
 require('express-mongoose');
-
 var routes = require('./routes/index');
 var middleware = require('./middleware/index');
 var mongoose = require('mongoose');
+var fs = require('fs');
 
 mongoose.connect(process.env.MONGOLAB_URI, function (err) {
     if (err) throw err;
@@ -17,13 +15,16 @@ mongoose.connect(process.env.MONGOLAB_URI, function (err) {
     middleware(app);
     routes(app);
 
-    var appPort = process.env.PORT;
-    if (appPort == undefined) {
-        appPort = 5000;
-    }
-    app.listen(appPort, function () {
-
     });
 
-    //mongoose.disconnect();
+    const https_port = 443;
+    require('https').createServer({
+        key: fs.readFileSync("./config/croc.space.key"),
+        cert: fs.readFileSync("./config/croc_space.crt"),
+        ca: fs.readFileSync("./config/croc_space.ca-bundle"),
+        requestCert: true,
+        rejectUnauthorized: false
+    }, app).listen(https_port, function () {
+        debug("HTTPS Server listening on port: %s, in %s mode", https_port, app.get('env'));
+    });
 });
