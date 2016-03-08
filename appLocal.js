13822695f58bb35494ceb60778ea4f731c4987b4
lzrmihnea <lazarm@MSGN02135.int.root.msg.ag>
2016-03-08 10:13:19 +0200

    Added register option and local launcher

---
+++ appLocal.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Mihnea on 5/1/2015.
+ */
+var express = require('express');
+
+// Add Mongoose query and promise to support to express
+require('express-mongoose');
+
+var routes = require('./routes/index');
+var middleware = require('./middleware/index');
+var mongoose = require('mongoose');
+
+const readOnlyUser_MongoLab = 'mongodb://localhost';
+
+mongoose.connect('mongodb://blog-user:Blog-Password0@ds059634.mongolab.com:59634/croc-blog', function (err) {
+    if (err) throw err;
+
+    var app = express();
+    middleware(app);
+    routes(app);
+
+    const PORT_NUMBER = 3000;
+
+    app.listen(PORT_NUMBER, function () {
+
+        //const SERVER_IP_ADDRESS = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+        const SERVER_IP_ADDRESS = 'http://localhost';
+        const LOCALHOST_LINK = SERVER_IP_ADDRESS + ":" + PORT_NUMBER;
+        const IRINA_LOGHIN = '/irinaloghin';
+
+        writeInitialConsoleMessage();
+
+        function writeInitialConsoleMessage() {
+            addNewlineToConsole();
+            console.log('Blog address: ' + LOCALHOST_LINK);
+            addNewlineToConsole();
+            console.log('Login backdoor ' + LOCALHOST_LINK + IRINA_LOGHIN);
+            addNewlineToConsole();
+            function addNewlineToConsole() {
+                console.log();
+            }
+        }
+    })
+    //mongoose.disconnect();
+});
--- routes/login.js
+++ routes/login.js
@@ -53,52 +53,54 @@ module.exports = function (app) {
         req.session.user = null;
         return res.redirect('/');
     });
-    //app.get('/register', function (req, res) {
-    //    res.render('register.jade');
-    //});
-    //
-    //app.post('/register', function (req, res, next) {
-    //    //var email = cleanString(req.params('email'));
-    //    //var pass = cleanString(req.params('pass'));
-    //    //TODO add cleanString
-    //    var email = req.body.email.toLowerCase();
-    //    var pass = req.body.pass;
-    //    if (!(email && pass)) {
-    //        return invalid();
-    //    }
-    //
-    //    User.findById(email, function (err, user) {
-    //        if (err) return next(err);
-    //
-    //        if (user) {
-    //            return res.render('register.jade', {exists: true});
-    //        }
-    //
-    //        crypto.randomBytes(16, function (err, bytes) {
-    //            if (err) return next(err);
-    //
-    //            var user = {_id: email};
-    //            user.salt = bytes.toString('utf8');
-    //            user.hash = hash(pass, user.salt);
-    //
-    //            User.create(user, function (err, newUser) {
-    //                if (err) {
-    //                    if (err instanceof mongoose.Error.ValidationError) {
-    //                        return invalid();
-    //                    }
-    //                    return next(err);
-    //                }
-    //
-    //                req.session.isLoggedIn = true;
-    //                req.session.user = email;
-    //                console.log('Created user: %s', email);
-    //                return res.redirect('/');
-    //            })
-    //        })
-    //    })
-    //    function invalid() {
-    //        return res.render('register.jade', {invalid: true});
-    //    }
-    //})
+    app.get('/register', function (req, res) {
+        res.render('register.jade');
+    });
+
+    app.post('/register', function (req, res, next) {
+        //var email = cleanString(req.params('email'));
+        //var pass = cleanString(req.params('pass'));
+        //TODO add cleanString
+        var email = req.body.email.toLowerCase();
+        var pass = req.body.pass;
+        var name = req.body.name;
+        if (!(email && pass)) {
+            return invalid();
+        }
+
+        User.findById(email, function (err, user) {
+            if (err) return next(err);
+
+            if (user) {
+                return res.render('register.jade', {exists: true});
+            }
+
+            crypto.randomBytes(16, function (err, bytes) {
+                if (err) return next(err);
+
+                var user = {_id: email};
+                user.salt = bytes.toString('utf8');
+                user.hash = hash(pass, user.salt);
+                user.name = name;
+
+                User.create(user, function (err, newUser) {
+                    if (err) {
+                        if (err instanceof mongoose.Error.ValidationError) {
+                            return invalid();
+                        }
+                        return next(err);
+                    }
+
+                    req.session.isLoggedIn = true;
+                    req.session.user = email;
+                    console.log('Created user: %s', email);
+                    return res.redirect('/');
+                })
+            })
+        })
+        function invalid() {
+            return res.render('register.jade', {invalid: true});
+        }
+    })
 
 }
