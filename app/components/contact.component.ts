26759b27006a8840bbc2fde49ca92c8fe7c10fa9
Mihnea <lzr_mihnea@yahoo.com>
2016-07-09 13:29:29 +0300

    Two-way databinding added Styling added for contact form

--- app/components/contact.component.ts
+++ app/components/contact.component.ts
@@ -3,13 +3,36 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'contact',
     template: `
-            <input [(ngModel)] = "contact.firstName" type="text">
             <div>
-                Phone number: {{contact.phone}}<br>
-                Email: {{contact.email}}
+                <div>
+                 <label for="first-name">First Name:</label>
+                    <input [(ngModel)] = "contact.firstName" type="text" id="first-name">
+                </div>
+                <div>
+                     <label for="last-name">Last Name:</label>    
+                     <input [(ngModel)] = "contact.lastName" type="text" id="last-name">
+                </div> 
+                <div>       
+                     <label for="phone">Phone number:</label>
+                    <input [(ngModel)] = "contact.phone" type="text" id="phone">
+                </div>
+                <div>
+                    <label for="email">Email:</label>
+                    <input [(ngModel)] = "contact.email" type="text" id="email">
+                </div>
             </div>     
             `,
-    inputs: ["contact"]
+    inputs: ["contact"],
+    styles: [`
+        label {
+        display: inline-block;
+        width: 140px;
+        }
+        
+        input {
+            width: 250px;
+        }
+    `]
 })
 
 export class ContactComponent {
